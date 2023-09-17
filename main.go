990d3738e21739c451abb86fdfa5921bcee5e76d
ErrNotNill <andreevivan8891@gmail.com>
2023-09-17 11:06:35 +0300

    fixed port

--- main.go
+++ main.go
@@ -3,19 +3,17 @@ package main
 import (
 	"avito_bitrix/avito"
 	"avito_bitrix/avito/handler"
+	"fmt"
 	"net/http"
 )
 
 func main() {
-
 	handler.InitDB(`mysqld:mysql@tcp(45.141.79.120:3306)/Onviz`)
 	//handler.CreateAccessToken()
 	//urlDb := os.Getenv("URL_MYSQL")
 	//bitrixDomain := os.Getenv("URL_MYSQL")
-
 	avito.AvitoRouter()
 	//handler.SetNotificationEnabled(Token)
-
-	http.ListenAndServe(":8080", nil)
-
+	fmt.Println("server started")
+	http.ListenAndServe(":9090", nil)
 }
