28d255c0fe69247d8a3d4b73dfb2d1003d96fe63
unknown <andreevivan8891@gmail.com>
2023-09-18 13:10:25 +0300

    some fixes

--- api/smart.go
+++ api/smart.go
@@ -11,8 +11,8 @@ func AddSmartProcess(title string, smartId int, address string, phone string, cl
 	phoneField := FindCustomFields("Телефон")
 	addressField := FindCustomFields("Адрес")
 	clientNameField := FindCustomFields("Имя")
-	appliesIdField := FindCustomFields("Откликнувшийся ID")
-	chatIdField := FindCustomFields("Чат ID")
+	appliesIdField := FindCustomFields("Откликнувшийся")
+	chatIdField := FindCustomFields("Чат")
 	titleVacancyField := FindCustomFields("Название")
 	newReq := fmt.Sprintf(`{
 "entityTypeId":%v,
@@ -27,7 +27,7 @@ func AddSmartProcess(title string, smartId int, address string, phone string, cl
 }
 }`, smartId, title, addressField, address, phoneField, phone, clientNameField, clientName, appliesIdField, appliesId, chatIdField, chatId, titleVacancyField, title) //UF_CRM_18_1694959668872: address, UF_CRM_18_1694004785: phone, UF_CRM_18_1695016806010: name
 	tr := bytes.NewReader([]byte(newReq))
-	fmt.Println("newReq Url: ", newReq)
+
 	webhookUrl := FindSettings("WebHookBitrixUrl")
 	endUrl := webhookUrl + "crm.item.add"
 	_, err := http.Post(endUrl, "application/json", tr)
